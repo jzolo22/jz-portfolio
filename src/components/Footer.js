a27351898ca6b0cec15a48554343710705e2d41a
jzolo22 <jzolotarev@gmail.com>
2021-02-24 21:39:45 -0500

    styled image component created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react'
+// import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import StyledImg from './StyledImg'
 
 function Footer(){
+
+    const StyledDiv = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;    
+    align-items: center;
+    `
+
     return (
-        <div className="footer"> Julia Zolotarev © 2021 </div>
+        <StyledDiv> 
+
+            <StyledImg src="/images/githubIcon.png" alt="github logo"/>
+
+            <div>
+                Julia Zolotarev © 2021 
+            </div>
+        
+        </StyledDiv>
     )
 }
 
---
+++ src/components/StyledImg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledImg = styled.img`
+    
+
+`
+
+export default StyledImg
