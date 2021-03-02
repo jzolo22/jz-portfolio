c492e3480dcdc51c613a672b724a86004fa4386a
jzolo22 <jzolotarev@gmail.com>
2021-03-02 16:00:44 -0500

    add styled component name

--- src/components/NameBanner.js
+++ src/components/NameBanner.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
+const Name = styled.h1`
+    text-align: center;
+    margin:auto
+`
 
 function NameBanner(){
 
     return (
         <div>
             <h3>Hi! I'm</h3>
-            <h1>Julia</h1>
+            <Name>Julia</Name>
         </div>
     )
 }
