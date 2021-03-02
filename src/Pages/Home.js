f7573098caca0ec3ccef71c2623b60f425c927e2
jzolo22 <jzolotarev@gmail.com>
2021-03-02 15:57:45 -0500

    extract namebanner to separate file

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import NameBanner from '../components/NameBanner'
 
 
 function Home(){
 
     return (
-        <div>
-            <h3>Hi! I'm</h3>
-            <h1>Julia</h1>
-        </div>
+        <NameBanner />
     )
 }
 
