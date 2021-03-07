d36fbec7d09dee119bb81215f5d8d58cdcc61437
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-06 19:27:23 -0500

    fix React noreferrer warning for links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,23 +15,23 @@ function Footer(){
 
     return (
         <StyledDiv> 
-            <a target="_blank" href="https://github.com/jzolo22" >
+            <a target="_blank" rel="noreferrer" href="https://github.com/jzolo22" >
                 <StyledImg src="/images/githubIcon.png" alt="github logo"/>
             </a>
             
-            <a target="_blank" href="https://www.linkedin.com/in/jzolotarev/" >
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jzolotarev/" >
                 <StyledImg src="/images/linkedinIcon.png" alt="linkedin logo"/>
             </a>
 
-            <a target="_blank" href="https://twitter.com/JZolo" >
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/JZolo" >
                 <StyledImg src="/images/twitterIcon.png" alt="twitter logo"/>
             </a>  
 
-            <a target="_blank" href="https://jzolotarev.medium.com/" >
+            <a target="_blank" rel="noreferrer" href="https://jzolotarev.medium.com/" >
                 <StyledImg src="/images/mediumIcon.png" alt="medium logo"/>
             </a>   
 
-            <a target="_blank" href="https://www.youtube.com/channel/UCEZuU86dwUCBBkLUr24e6GA/videos" >
+            <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCEZuU86dwUCBBkLUr24e6GA/videos" >
                 <StyledImg src="/images/youtubeIcon.png" alt="medium logo"/>
             </a>        
 
